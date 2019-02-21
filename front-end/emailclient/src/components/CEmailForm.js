e8983acb762a4d56720314760b0095b3f0caf36b
yufrances90 <yufrances90@gmail.com>
2019-02-21 11:26:47 -0800

    feat: Prepared email content to be sent

--- front-end/emailclient/src/components/CEmailForm.js
+++ front-end/emailclient/src/components/CEmailForm.js
@@ -1,34 +1,63 @@
 import React, { Component } from 'react';
 
 import {
-    TextField
+    TextField,
+    Button
 } from '@material-ui/core';
 
 class CEmailForm extends Component {
+
+    handleChange(event) {
+        this.props.handleUpdate(event);
+    }
+
     render() {
+
+        const { 
+            email, 
+            title, 
+            body,
+            handleSubmit 
+        } = this.props;
+
         return (
             <form>
                 <TextField
                     id="email"
+                    name="email"
                     label="To"
                     fullWidth
+                    onChange={this.handleChange.bind(this)}
+                    value={email}
                 />
                 <br />
                 <TextField
                     id="title"
+                    name="title"
                     label="Subject"
                     fullWidth
+                    onChange={this.handleChange.bind(this)}
+                    value={title}
                 />
                 <br />
                 <br />
                 <TextField
                     id="body"
+                    name="body"
                     label="Body"
                     fullWidth
                     multiline
                     rows="10"
                     variant="outlined"
+                    onChange={this.handleChange.bind(this)}
+                    value={body}
                 />
+                <Button 
+                    className="submit-btn"
+                    onClick={handleSubmit}
+                >
+                    Send
+                </Button>
             </form>
         );
     }
--- front-end/emailclient/src/pages/PCreate.js
+++ front-end/emailclient/src/pages/PCreate.js
@@ -2,10 +2,46 @@ import React, { Component } from 'react';
 import CEmailForm from '../components/CEmailForm';
 
 class PCreate extends Component {
+
+    state = {
+        email: "",
+        title: "",
+        body: ""
+    }
+
+    handleUpdate(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+
+        const { email, title, body } = this.state;
+
+        const obj = {
+            receiver: email,
+            sender: "test@yahoo.ca",
+            title,
+            body
+        }
+
+        console.log(obj);
+    }
+
     render() {
+
+        const { email, title, body } = this.state;
+
         return (
             <div>
-                <CEmailForm />
+                <CEmailForm 
+                    email={email}
+                    title={title}
+                    body={body}
+                    handleUpdate={this.handleUpdate.bind(this)}
+                    handleSubmit={this.handleSubmit.bind(this)}
+                />
             </div>
         );
     }
