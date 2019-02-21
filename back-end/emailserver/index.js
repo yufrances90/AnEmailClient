8264c06f90301d283dec7a1a0312713935c0de70
yufrances90 <yufrances90@gmail.com>
2019-02-21 09:23:26 -0800

    feat: Added endpoint to save email

--- back-end/emailserver/index.js
+++ back-end/emailserver/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+})); // support encoded bodies
+
 const dbUtils = require('./utils/db')
 
 app.get('/', (req, res) => {
@@ -18,9 +24,22 @@ app.get('/db', (req, res) => {
             body: "This is a test!"
         }
     ]
-    
+
     dbUtils.insertIntoDatabase(emails);
 });
+
+app.post('/email', (req, res) => {
+
+    const emailsToInsert = req.body.emailsToInsert;
+
+    if (emailsToInsert === undefined || emailsToInsert.length == 0) {
+        res.status(500).send("Error: No emails are provided!");
+    } else {
+        res.send('Saving new emails...');
+    }
+
+    dbUtils.insertIntoDatabase(emailsToInsert);
+})
 
 app.listen(8000, () => {
     console.log('App listening on port 8000!')
